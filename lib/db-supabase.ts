23f12b5ccc098b6330540c02065dd64270d64050
Ismail Pehlivan <106168801+pehlivanismail@users.noreply.github.com>
2025-11-10 09:11:14 +0100

    Update db-supabase.ts

--- lib/db-supabase.ts
+++ lib/db-supabase.ts
@@ -49,7 +49,6 @@ export async function getQuestionsByLevel(userId: string, level: string) {
     .select('question_id, status, attempts')
     .eq('user_id', userId)
     .in('question_id', questionIds)
-    .eq('category_id', questions[0].category_id || null)
 
   if (progressError) {
     throw progressError
